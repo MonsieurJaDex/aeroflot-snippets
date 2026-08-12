138e6bd562a958756b11c7414d97345b8c6a3745
MonsieurJaDex <coder.dinya@gmail.com>
2026-08-12 18:23:08 +0300

    structure change

---
+++ backend/target/debug/aeroflot-snippets.d
@@ -0,0 +1,1 @@
+E:\Projects\aeroflot-snippets\backend\target\debug\aeroflot-snippets.exe: E:\Projects\aeroflot-snippets\backend\src\bfs.rs E:\Projects\aeroflot-snippets\backend\src\main.rs E:\Projects\aeroflot-snippets\backend\src\types.rs
---
+++ backend/target/debug/deps/aeroflot_snippets-036575b8e7b1eb33.d
@@ -0,0 +1,7 @@
+E:\Projects\aeroflot-snippets\backend\target\debug\deps\aeroflot_snippets-036575b8e7b1eb33.d: src\main.rs src\types.rs src\bfs.rs
+
+E:\Projects\aeroflot-snippets\backend\target\debug\deps\libaeroflot_snippets-036575b8e7b1eb33.rmeta: src\main.rs src\types.rs src\bfs.rs
+
+src\main.rs:
+src\types.rs:
+src\bfs.rs:
