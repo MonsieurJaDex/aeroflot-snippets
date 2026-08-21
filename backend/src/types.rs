965b1f0bb70406381a30fa95371f7be01ae62c86
Denis Medvedev <jadex@MacBook-Air-Denis.local>
2026-08-21 17:22:47 +0300

    added common errors, upd. stucts, parser function to be realised, 2 vers. of bfs realised (can be restructured from AoS to SoA format, already testing)

--- backend/src/types.rs
+++ backend/src/types.rs
@@ -1,3 +1,7 @@
+use std::error::Error;
+
+use crate::error;
+
 pub type MapMatrix<T> = Vec<Vec<T>>;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -6,5 +10,38 @@ pub struct Point<T>(pub T, pub T); // Point(x, y)
 impl<T> Point<T> {
     pub fn new(x: T, y: T) -> Self {
         Self(x, y)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum EngineerType {
+    Electronic,
+    Mechanical,
+}
+
+pub struct Engineers<T> {
+    pub points: Vec<Point<T>>,
+    pub types: Vec<EngineerType>,
+    pub tile: Vec<T>,
+}
+
+impl<T> Engineers<T> {
+    fn new(
+        _points: Vec<Point<T>>,
+        _types: Vec<EngineerType>,
+        _tile: Vec<T>,
+    ) -> Result<Self, Box<dyn Error>> {
+        let len_sum = _points.len() + _types.len() + _tile.len();
+        if len_sum == 0 || len_sum % 3 != 0 {
+            return Err(Box::new(error::CommonErrors::InvalidArgument(
+                "arguments should have equal length and more than a zero".to_string(),
+            )));
+        }
+
+        Ok(Self {
+            points: _points,
+            types: _types,
+            tile: _tile,
+        })
     }
 }
