fbf07bbf4a6b783769be6770635ec62d730dcd8d
Denis Medvedev <jadex@MacBook-Air-Denis.local>
2026-09-16 18:23:13 +0300

    active engineers route added

--- backend/src/types/doc.rs
+++ backend/src/types/doc.rs
@@ -6,13 +6,16 @@ use crate::router::{
         __path_get_engineer_name_handler, __path_login_handler, __path_register_handler,
         __path_update_access_token,
     },
-    simulate::{__path_get_engineers_positions, __path_update_engineer_position_handler},
+    simulate::{
+        __path_active_engineers, __path_get_engineers_positions,
+        __path_update_engineer_position_handler,
+    },
 };
 use crate::types::map::{MapMatrix, Point};
 
 #[derive(OpenApi)]
 #[openapi(
-    paths(get_map, get_route, assign_engineer, get_current_task, accept_current_task, login_handler, register_handler, update_access_token, get_engineer_name_handler, get_engineers_positions, update_engineer_position_handler),
+    paths(get_map, get_route, assign_engineer, get_current_task, accept_current_task, login_handler, register_handler, update_access_token, get_engineer_name_handler, get_engineers_positions, update_engineer_position_handler, active_engineers),
     components(schemas(
         MapMatrix,
         Point,
